7fc7d7f3d1b8dee5f45c43202c62b703cd665dbf
m43i <36700796+m43i@users.noreply.github.com>
2023-03-18 12:38:56 +0100

    Track watch time

--- bot-watch-time/src/main.rs
+++ bot-watch-time/src/main.rs
@@ -1,38 +1,121 @@
 use anyhow::{Error, Result};
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ChatterList {
-    broadcaster: Vec<String>,
-    vips: Vec<String>,
-    moderators: Vec<String>,
-    staff: Vec<String>,
-    admins: Vec<String>,
-    global_mods: Vec<String>,
-    viewers: Vec<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Chatters {
-    chatter_count: u32,
-    chatters: ChatterList,
-}
+use database::{entity::channel as channel_entity, sea_orm::ActiveValue};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenvy::dotenv().ok();
-    let client = reqwest::Client::new();
+    let client_id = std::env::var("TWITCH_CLIENT_ID").expect("TWITCH_CLIENT_ID not set");
+    let client_secret =
+        std::env::var("TWITCH_CLIENT_SECRET").expect("TWITCH_CLIENT_SECRET not set");
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
+    let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL not set");
+
+    let db = database::connect(&db_url).await?;
+    let mut redis = cache::connect(&redis_url).await?;
 
     loop {
-        let res = client
-            .get("https://tmi.twitch.tv/group/user/anniislost/chatters")
-            .send()
-            .await?
-            .json::<Chatters>()
-            .await?;
+        let bot = database::handler::bot::get_bot("dustin", &db).await?;
+        let token =
+            auth::token::get_bot_token("dustin", &client_id, &client_secret, &db, &mut redis)
+                .await?;
 
-        println!("{:?}", res);
+        let channels: Vec<channel_entity::Model> =
+            database::handler::channel::get_live_channels(&db).await?;
 
-        tokio::time::sleep(tokio::time::Duration::from_secs(10 * 60)).await;
+        for channel in channels {
+            let mut chatters: Vec<twitch_api::Chatter> = Vec::new();
+            match twitch_api::get_chatters(
+                &client_id,
+                &token,
+                bot.twitch_id,
+                channel.id,
+                "",
+                &mut chatters,
+            )
+            .await
+            {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    continue;
+                }
+            };
+
+            let mut users: Vec<database::entity::user::ActiveModel> = Vec::new();
+            for chatter in chatters.clone() {
+                let user_id = i32::from_str_radix(&chatter.user_id, 10).unwrap();
+                let user = database::entity::user::ActiveModel {
+                    id: ActiveValue::Set(user_id),
+                    nick: ActiveValue::Set(chatter.user_login),
+                    display_name: ActiveValue::Set(chatter.user_name),
+                    ..Default::default()
+                };
+
+                users.push(user);
+            }
+            match database::handler::user::create_many(users, &db).await {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    continue;
+                }
+            };
+
+            let currently_watching =
+                database::handler::watchtime::get_currently_watching(channel.id, &db).await;
+            let currently_watching = match currently_watching {
+                Ok(watching) => watching,
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    continue;
+                }
+            };
+
+            let no_longer_watching = currently_watching
+                .iter()
+                .filter(|user| {
+                    !chatters.iter().any(|chatter| {
+                        i32::from_str_radix(&chatter.user_id, 10).unwrap() == user.user_id
+                    })
+                })
+                .map(|user| user.user_id.clone())
+                .collect::<Vec<i32>>();
+            let new_watching = chatters
+                .iter()
+                .map(|chatter| i32::from_str_radix(&chatter.user_id, 10).unwrap())
+                .filter(|chatter| {
+                    !currently_watching
+                        .iter()
+                        .any(|user| chatter == &user.user_id)
+                })
+                .collect::<Vec<i32>>();
+
+            if no_longer_watching.len() > 0 {
+                match database::handler::watchtime::stop_watching(
+                    channel.id,
+                    no_longer_watching,
+                    &db,
+                )
+                .await
+                {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                    }
+                };
+            }
+            if new_watching.len() > 0 {
+                match database::handler::watchtime::start_watching(channel.id, new_watching, &db)
+                    .await
+                {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                    }
+                };
+            }
+        }
+
+        tokio::time::sleep(std::time::Duration::from_secs(2 * 60)).await;
     }
 }
